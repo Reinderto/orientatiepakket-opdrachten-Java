f7a903605dd7ac04d24bb9298296e3da8461c0e0
Reindert <rovervest@outlook.com>
2024-02-13 16:37:36 +0100

    Day 4 and 5.1

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,32 @@
+//import java.io.Console;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
-        System.out.println("Bread is ready.");
-        System.out.println("Who is the bread for?");
-        String name = reader.nextLine();
-        System.out.println("Noted: " + name + " got bread.");
+        char letter = 'a';
+        boolean bool = true;
+        long bigNumber = 64;
+        int normalNumber = 32;
+        short kortNumber = 16;
+        byte heelKortNumber = 8;
+        float kommaNumber = 3.2f;
+        double anderKommaNumber = 4.9;
+
+        letter = 'b';
+        bool = false;
+        bigNumber = 15145145;
+        normalNumber = 1415;
+        kortNumber = 578;
+        heelKortNumber = 54;
+        kommaNumber = 135.54f;
+        anderKommaNumber = 51.54;
+
+        System.out.println(letter);
+        System.out.println(bool);
+        System.out.println(bigNumber);
+        System.out.println(normalNumber);
+        System.out.println(kortNumber);
+        System.out.println(heelKortNumber);
+        System.out.println(kommaNumber);
+        System.out.println(anderKommaNumber);
     }
 }
