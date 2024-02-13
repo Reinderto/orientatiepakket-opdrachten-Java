682cc33ed3574fa4882ffe5cb7a3cb50e36e3620
Reindert <rovervest@outlook.com>
2024-02-13 16:47:56 +0100

    Day 5.2

--- src/Main.java
+++ src/Main.java
@@ -2,31 +2,12 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        char letter = 'a';
-        boolean bool = true;
-        long bigNumber = 64;
-        int normalNumber = 32;
-        short kortNumber = 16;
-        byte heelKortNumber = 8;
-        float kommaNumber = 3.2f;
-        double anderKommaNumber = 4.9;
-
-        letter = 'b';
-        bool = false;
-        bigNumber = 15145145;
-        normalNumber = 1415;
-        kortNumber = 578;
-        heelKortNumber = 54;
-        kommaNumber = 135.54f;
-        anderKommaNumber = 51.54;
-
-        System.out.println(letter);
-        System.out.println(bool);
-        System.out.println(bigNumber);
-        System.out.println(normalNumber);
-        System.out.println(kortNumber);
-        System.out.println(heelKortNumber);
-        System.out.println(kommaNumber);
-        System.out.println(anderKommaNumber);
+        Scanner reader = new Scanner(System.in);
+        System.out.println("What is the base size of the triangle");
+        double base = reader.nextDouble();
+        System.out.println("What is the height of the triangle");
+        double height = reader.nextDouble();
+        double area = base * height / 2;
+        System.out.println("the area of the triangle is " + area);
     }
 }
