ad4553dbb16191428fa6e660b5f200d37b54db91
Reindert <rovervest@outlook.com>
2024-02-14 17:31:08 +0100

    Day 9

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,16 @@ public class Main {
         String resetColor = "\u001B[0m";
         String redColor = "\u001b[31m";
         Scanner reader = new Scanner(System.in);
-        System.out.println("Target Row?");
-        int row = reader.nextInt();
-        System.out.println("Target Column?");
-        int column = reader.nextInt();
-        System.out.println("Deploy to:");
-        System.out.println(redColor + "(" + (row + 1) + ", " + column + ")" + resetColor);
-        System.out.println(redColor + "(" + (row - 1) + ", " + column + ")" + resetColor);
-        System.out.println(redColor + "(" + row + ", " + (column + 1) + ")" + resetColor);
-        System.out.println(redColor + "(" + row + ", " + (column - 1) + ")" + resetColor);
-        Toolkit.getDefaultToolkit().beep();
+        System.out.println("Input number");
+        int number = reader.nextInt();
+        if(number % 2 == 0)
+        {
+            System.out.println("Tick");
+        }
+        else
+        {
+            System.out.println("Tock");
+        }
+
     }
 }
