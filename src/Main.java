a392e68c30d49bca449a5345bedd7ed42e52a015
Reindert <rovervest@outlook.com>
2024-02-14 18:32:12 +0100

    Day 11

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,42 @@
-import java.awt.*;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        String resetColor = "\u001B[0m";
-        String redColor = "\u001b[31m";
         Scanner reader = new Scanner(System.in);
-        System.out.println("Input number");
-        int number = reader.nextInt();
-        if(number % 2 == 0)
+        Tuple<?, ?>[] items = new Tuple[]{
+                new Tuple<>("Rope", 10),
+                new Tuple<>("Torches", 15),
+                new Tuple<>("Climbing Equipment", 25),
+                new Tuple<>("Clean Water", 1),
+                new Tuple<>("Machete", 20),
+                new Tuple<>("Canoe", 200),
+                new Tuple<>("Food Supplies", 1)
+        };
+        String discountName = "Reindert";
+        System.out.println("The following items are available:");
+        System.out.println();
+        for(int i = 0;i < items.length;i++)
         {
-            System.out.println("Tick");
+            System.out.println(i + 1 + ". " + items[i].Item1);
+        }
+        System.out.println();
+        System.out.println("What number do you want to see the price of?");
+        int number = reader.nextInt();
+        reader.nextLine();
+        if(number <= 0 || number > items.length)
+        {
+            System.out.println("There is no item number " + number);
         }
         else
         {
-            System.out.println("Tock");
+            int price = (int) items[number - 1].Item2;
+            System.out.println("What is your name?");
+            String name = reader.nextLine();
+            if(discountName.equals(name))
+            {
+                price /= 2;
+            }
+            System.out.println(items[number - 1].Item1 + " cost " + price + " gold.");
         }
-
     }
 }
+
---
+++ src/Tuple.java
@@ -0,0 +1,10 @@
+public class Tuple<X, Y>
+{
+    public X Item1;
+    public Y Item2;
+    public Tuple(X item1, Y item2)
+    {
+        Item1 = item1;
+        Item2 = item2;
+    }
+}
