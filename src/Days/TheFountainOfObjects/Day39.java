e7eedbbc7878aac14c94ceb2065bea391c6e5e23
Reindert <rovervest@outlook.com>
2024-03-04 17:22:41 +0100

    Day 40

--- src/Days/TheFountainOfObjects/Day39.java
+++ src/Days/TheFountainOfObjects/Day39.java
@@ -2,6 +2,8 @@ package Days.TheFountainOfObjects;
 
 import Days.DayOpdracht;
 
+import java.util.HashSet;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Day39 extends DayOpdracht {
@@ -10,14 +12,16 @@ public class Day39 extends DayOpdracht {
     public int Row = 0;
     public int Column = 0;
     public Room[][] World;
+    public static int PitFrequency = 16;
     @Override
     public void Run() {
-        int size = 4;
+        int size = AskForNumberInRange("How big do you want the world to be? (min 4)", 4, 128);
         GenerateWorld(size);
         Scanner reader = new Scanner(System.in);
         while (gameIsActive){
             System.out.println("You are in the room at (Row=" + Row + ", Column=" + Column + ").");
             System.out.println(World[Row][Column].RoomDescription());
+            PrintAdjacentRoomDescriptions();
             System.out.println("What do you want to do?");
             String action = reader.nextLine();
             if(action.startsWith("move ")){
@@ -29,6 +33,27 @@ public class Day39 extends DayOpdracht {
             }
             System.out.println("----------------------------------------------------------------------------------");
         }
+    }
+
+    private void PrintAdjacentRoomDescriptions(){
+        HashSet<String> adjacentRooms = new HashSet<>();
+        for(int i = -1;i <= 1; i++){
+            for (int j = -1; j <= 1;j++){
+                if((i != 0 || j != 0) && IsInWorld(Row + i, Column + j)){
+                    String adjacentRoomDesc = World[Row + i][Column + j].AdjacentDescription();
+                    if(!adjacentRoomDesc.isEmpty()){
+                        adjacentRooms.add(adjacentRoomDesc);
+                    }
+                }
+            }
+        }
+        if(!adjacentRooms.isEmpty()){
+            System.out.println(String.join("\n", adjacentRooms));
+        }
+    }
+
+    private boolean IsInWorld(int row, int column){
+        return row >= 0 && row < World.length && column >= 0 && column < World.length;
     }
 
     private void Move(String direction){
@@ -72,6 +97,31 @@ public class Day39 extends DayOpdracht {
             }
         }
         World[0][0] = new EntranceRoom();
-        World[0][2] = new FountainRoom();
+        if(size == 4) {
+            World[0][2] = new FountainRoom();
+        }else {
+            Random rand = new Random();
+            int row = rand.nextInt(size - 2) + 2;
+            int column = rand.nextInt(size - 2) + 2;
+            System.out.println("the fountain is at (Row=" + row + ", Column=" + column + ").");
+            PlaceRoom(new FountainRoom(), row, column);
+        }
+        for(int i = 0; i < World.length * World.length / PitFrequency;i++){
+            PlaceRoom(new PitRoom());
+        }
+    }
+
+    private void PlaceRoom(Room room){
+        Random rand = new Random();
+        int row, column;
+        do{
+            row = rand.nextInt(World.length-2) + 2;
+            column = rand.nextInt(World.length-2)+2;
+        }while (World[row][column].equals(new EmptyRoom()));
+        PlaceRoom(room, row, column);
+    }
+
+    private void PlaceRoom(Room room, int row, int column){
+        World[row][column] = room;
     }
 }
---
+++ src/Days/TheFountainOfObjects/PitRoom.java
@@ -0,0 +1,19 @@
+package Days.TheFountainOfObjects;
+
+public class PitRoom extends Room{
+    @Override
+    public String RoomDescription() {
+        return "this is a pit";
+    }
+
+    @Override
+    public void OnEnterRoom(Day39 game) {
+        game.gameIsActive = false;
+        System.out.println("You fell into a Pit!\nYou lose");
+    }
+
+    @Override
+    public String AdjacentDescription() {
+        return "You feel a draft. There is a pit in a nearby room.";
+    }
+}
